baca02f2b8e778e0a6a900f76682e6dc2c143084
Linnxw <120721205+Linnxw@users.noreply.github.com>
2023-03-03 16:54:55 +0700

    delete reducer.js (salah save file)

--- Reducer.js
+++
@@ -1,9 +0,0 @@
-export default function Reducer(){
-  return (
-    <div className="h-20 w-60 bg-sky-500 rounded-lg shadow-2xl mx-auto">
-     <span className="inline-block h-full w-1/3 bg-slate-200 text-slate-700 text-center">+</span>
-     <span>0</span>
-     <span>-</span>
-    </div>
-    )
-}
